039ab7b52bcd5d6da5f1812513275e2a44f71035
QuangHuy2201 <huyq706@gmail.com>
2023-08-07 18:16:57 +0700

    add logging page

--- html/new_loging.php
+++ html/new_loging.php
@@ -2,54 +2,46 @@
 include "../view/header.php";
 ?>
 
-<div class="container">
-    <div class="row bg-white h-100">
-        <div class="col-4">
-            <img src="../static/images/logo/login_img.jpg" alt="logging">
-        </div>
-        <div class="col-8 my-auto plr50">
-            <form action="">
-                <!-- Email input -->
-                <div class="mb-4">
-                    <label class="d-block mb-2" for="email">Email address</label>
-                    <input type="email" id="email" class="p10 w-100 text-start" />
-                </div>
-
-                <!-- Password input -->
-                <div class="form-outline mb-4">
-                    <label class="d-block mb-2" for="password">Password</label>
-                    <input type="password" id="password" class="p10 w-100 text-start" />
-                </div>
-
-                <!-- 2 column grid layout for inline styling -->
-                <div class="row mb-4">
-                    <div class="col d-flex justify-content-center">
-                        <!-- Checkbox -->
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                            <label class="form-check-label" for="form2Example31"> Remember me </label>
-                        </div>
+<section class="myform-area">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="form-area login-form">
+                    <div class="form-content">
+                        <img src="../static/images/logo/login_img.jpg" alt="logging" >
                     </div>
-                    <div class="col  d-flex justify-content-center">
-                        <!-- Simple link -->
-                        <a href="#!">Forgot password?</a>
+                    <div class="form-input">
+                        <h2>Đăng Nhập</h2>
+                        <form>
+                            <div class="form-group">
+                                <input type="email" id="" name="name" required>
+                                <label>Email</label>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" id="" name="password" required>
+                                <label>Mật khẩu</label>
+                            </div>
+                            <div class="forgot-password fs14 text-end mt10">
+                                <a href="#">Quên mật khẩu?</a>
+                            </div>
+                            <div class="myform-button">
+                                <button class="myform-btn">Đăng nhập</button>
+                            </div>
+                            <div class="register fs14 text-center mt20">
+                                Bạn chưa phải là thành viên?
+                                <a href="#" class="d-block">Đăng ký</a>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <div class=" d-flex justify-content-center">
-                    <button type="button" class="btn color-primary mb-4 text-white fs16 plr20 pt05 pb05">Sign in</button>
-                </div>
-
-                <!-- Register buttons -->
-                <div class="text-center">
-                    <p>Not a member? <a href="#!">Register</a></p>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
-    </body>
+</section>
+</body>
 
-    </html>
+</html>
 
-    <?php
-    include "../view/footer.php";
-    ?>
+<?php
+include "../view/footer.php";
+?>
