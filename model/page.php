d9e079df4561dcfe64b21e6660337fb538325bed
BinhNguyen2806 <93757096+BinhNguyen2806@users.noreply.github.com>
2023-08-10 07:38:26 +0700

    Fix sorting with category=0

--- model/page.php
+++ model/page.php
@@ -252,22 +252,22 @@ function page_bar_category($page_current,$page_left,$page_right,$pages,$category
     {
         if($page_current!=1)
     {
-        echo' <a href="index.php?act=product&category='.$category_id.'&page=1" class="btn btn-outline-secondary"><<</a> ';
+        echo' <a href="index.php?act=product&page=1" class="btn btn-outline-secondary"><<</a> ';
         if($page_current-3>1)
         echo' <a href="" class=" disabled btn btn-outline-secondary">...</a> ';
     }
     
     for($i=$page_left ;$i<=$page_right;$i++ )
     {   if ($page_current==$i)
-        echo' <a href="index.php?act=product&category='.$category_id.'&page='.$i.'" class="active btn btn-outline-secondary">'.$i.'</a>';
+        echo' <a href="index.php?act=product&page='.$i.'" class="active btn btn-outline-secondary">'.$i.'</a>';
         else
-        echo' <a href="index.php?act=product&category='.$category_id.'&page='.$i.'" class="btn btn-outline-secondary">'.$i.'</a>';
+        echo' <a href="index.php?act=product&page='.$i.'" class="btn btn-outline-secondary">'.$i.'</a>';
     }
     if($page_current<$pages)
     {
     if($page_current+3<$pages)
     echo' <a href="" class=" disabled btn btn-outline-secondary">...</a> ';
-    echo'<a href="index.php?act=product&category='.$category_id.'&page='.$pages.'" class="btn btn-outline-secondary">>></a> ';
+    echo'<a href="index.php?act=product&page='.$pages.'" class="btn btn-outline-secondary">>></a> ';
     }
     }
 }          
