0827a37e124e946983c6cec0406794aef06de18d
BinhNguyen2806 <93757096+BinhNguyen2806@users.noreply.github.com>
2023-08-13 20:29:44 +0700

    Fix header and unset on one page

--- view/myaccount.php
+++ view/myaccount.php
@@ -1,15 +1,8 @@
 
 <?php
 if(isset($_SESSION['msg']))
-{
-    echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
-              '.$_SESSION['msg'].'
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>'; 
-    unset($_SESSION['msg']);
-}
+{show_to_warning($_SESSION['msg']);unset($_SESSION['msg']);}
 ?>
-
 <div class="container-xl px-4 mt-4">
     <!-- Account page navigation-->
     <nav class="nav nav-borders">
